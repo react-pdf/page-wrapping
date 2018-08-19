e8cd635d693cbbf5f53f98b8a4aa4ed2874e6b31
Diego Muracciole <diegomuracciole@gmail.com>
2018-08-19 02:21:23 -0300

    Add tests setup

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  modulePathIgnorePatterns: ['/node_modules/'],
+  moduleDirectories: ['node_modules'],
+  testRegex: 'test/.*?\\.js',
+};
---
+++ test/index.js
@@ -0,0 +1,5 @@
+describe('page-wrapping', () => {
+  test('Should run tests', () => {
+  
+  });
+});
